5606eb5bff47af39d5422fad5489141e5bee263b
KAVITAPISAL <85871081+KAVITAPISAL@users.noreply.github.com>
2023-05-18 00:05:28 +0530

    update

---
+++ main_app/models.py
@@ -0,0 +1,269 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import UserManager
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = CustomUser(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        assert extra_fields["is_staff"]
+        assert extra_fields["is_superuser"]
+        return self._create_user(email, password, **extra_fields)
+
+
+class Session(models.Model):
+    start_year = models.DateField()
+    end_year = models.DateField()
+
+    def __str__(self):
+        return "From " + str(self.start_year) + " to " + str(self.end_year)
+
+
+class CustomUser(AbstractUser):
+    USER_TYPE = ((1, "HOD"), (2, "Staff"), (3, "Student"),(3, "Librarian"))
+    GENDER = [("M", "Male"), ("F", "Female")]    
+    username = models.CharField(max_length=250,blank=True,null=True )  # Removed username, using email instead
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(default=1, choices=USER_TYPE, max_length=1)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    profile_pic = models.ImageField()
+    address = models.TextField()
+    fcm_token = models.TextField(default="")  # For firebase notifications
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
+
+
+class Admin(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+class Course(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE,related_name="admin_name")
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=True, blank=False)
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING, null=True)
+    student_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE,related_name="student_id")
+
+    def __str__(self):
+        return self.admin.last_name + ", " + self.admin.first_name
+
+
+class Staff(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=True, blank=False)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
+class Librarian(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=120)
+    staff = models.ForeignKey(Staff,on_delete=models.CASCADE,)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Attendance(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING)
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class AttendanceReport(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    attendance = models.ForeignKey(Attendance, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class LeaveReportStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class LeaveReportStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeedbackStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeedbackStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class NotificationStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class NotificationStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StudentResult(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    test = models.FloatField(default=0)
+    exam = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            Admin.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Staff.objects.create(admin=instance)
+        if instance.user_type == 3:
+            Student.objects.create(admin=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.admin.save()
+    if instance.user_type == 2:
+        instance.staff.save()
+    if instance.user_type == 3:
+        instance.student.save()
+
+
+
+
+# library
+
+
+
+class Author(models.Model):
+    name=models.CharField(max_length=350)
+    description=models.CharField(max_length=450)
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    name=models.CharField(max_length=350)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    image=models.ImageField()
+    category=models.CharField(max_length=220)
+
+    def __str__(self):
+        return self.name
+    
+class Issue(models.Model):
+    student=models.ForeignKey(Student,on_delete=models.CASCADE)
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    created_at=models.DateTimeField( auto_now=True)
+    issued=models.BooleanField(default=False)
+    issued_at=models.DateTimeField( auto_now=False,null=True,blank=True)
+    returned=models.BooleanField(default=False)
+    return_date=models.DateTimeField(auto_now=False,auto_created=False,auto_now_add=False,null=True,blank=True)
+
+    def __str__(self):
+        return "{}_{} book issue request".format(self.student,self.book)
+
+    def days_no(self):
+        "Returns the no. of days before returning / after return_date."
+        if self.issued:
+            y,m,d=str(timezone.now().date()).split('-')
+            today=datetime.date(int(y),int(m),int(d))
+            y2,m2,d2=str(self.return_date.date()).split('-')
+            lastdate=datetime.date(int(y2),int(m2),int(d2))
+            print(lastdate-today,lastdate>today)
+            if lastdate > today:
+                return "{} left".format(str(lastdate-today).split(',')[0])
+            else:
+                return "{} passed".format(str(today-lastdate).split(',')[0])
+        else:
+            return ""
+    
+class Fine(models.Model):
+    student=models.ForeignKey(Student,on_delete=models.CASCADE)
+    issue=models.ForeignKey(Issue,on_delete=models.CASCADE)
+    amount=models.DecimalField(default=0.00,max_digits=10,decimal_places=2)
+    paid=models.BooleanField(default=False)
+    order_id = models.CharField(unique=True, max_length=500, null=True, blank=True, default=None) 
+    datetime_of_payment = models.DateTimeField(auto_now=False,null=True,blank=True)
+    
+    # related to razorpay
+    razorpay_order_id = models.CharField(max_length=500, null=True, blank=True)
+    razorpay_payment_id = models.CharField(max_length=500, null=True, blank=True)
+    razorpay_signature = models.CharField(max_length=500, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.order_id is None :
+            self.order_id = "{}_{}_{}".format(self.student.department,self.student.student_id.username,timezone.now().strftime('%H%M%S') )  
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "{} fine->{}".format(self.issue,self.amount)
